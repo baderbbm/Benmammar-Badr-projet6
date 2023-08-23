056a6998afe02ab57ab3a86923514604fa6ee37b
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-23 23:35:31 +0200

    essai5

--- src/main/java/com/openclassrooms/DataLayerSec/service/UtilisateurService.java
+++ src/main/java/com/openclassrooms/DataLayerSec/service/UtilisateurService.java
@@ -1,10 +1,16 @@
 package com.openclassrooms.DataLayerSec.service;
  
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import com.openclassrooms.DataLayerSec.model.NatureOperation;
+import com.openclassrooms.DataLayerSec.model.Operation;
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
+import com.openclassrooms.DataLayerSec.repository.OperationRepository;
 import com.openclassrooms.DataLayerSec.repository.UtilisateurRepository;
  
 @Service
@@ -12,6 +18,10 @@ public class UtilisateurService {
  
 	@Autowired
 	private UtilisateurRepository utilisateurRepository;
+	
+	
+	@Autowired
+	private OperationRepository operationRepository;
 	
 	public Iterable<Utilisateur> getUsers() {
 		return utilisateurRepository.findAll();
@@ -62,5 +72,40 @@ public class UtilisateurService {
 	        utilisateur.getAmis().add(ami);
 	        utilisateurRepository.save(utilisateur);
 	    }
-	 
+	    
+	    public void effectuerDepot(Utilisateur utilisateur, BigDecimal montant) {
+	        utilisateur.effectuerDepot(montant);
+	        utilisateurRepository.save(utilisateur);
+	        
+	        Operation operation = new Operation();
+	        operation.setUtilisateur(utilisateur);
+	        operation.setMontant(montant);
+	        operation.setDateetheureoperation(LocalDateTime.now());
+	        operation.setNatureoperation(NatureOperation.depot);
+	        operationRepository.save(operation);
+	    }
+	    
+		// exception personnalisée 
+	    public class InsufficientBalanceException extends RuntimeException {
+	        public InsufficientBalanceException(String message) {
+	            super(message);
+	        }
+	    }
+
+	    
+	    public void effectuerRetrait(Utilisateur utilisateur, BigDecimal montant) {
+	        if (utilisateur.getSoldeDuCompte().compareTo(montant) >= 0) {
+	            utilisateur.effectuerRetrait(montant);
+	            utilisateurRepository.save(utilisateur);
+
+	            Operation operation = new Operation();
+	            operation.setUtilisateur(utilisateur);
+	            operation.setMontant(montant);
+	            operation.setDateetheureoperation(LocalDateTime.now());
+	            operation.setNatureoperation(NatureOperation.retrait);
+	            operationRepository.save(operation);
+	        } else {
+	            throw new InsufficientBalanceException("Solde insuffisant pour effectuer le retrait.");
+	        }
+	    }	 
 }
