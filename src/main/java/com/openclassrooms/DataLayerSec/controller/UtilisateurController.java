c5fca87a819275c7992eaa7729275b9de7565722
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-23 23:36:01 +0200

    essai5

--- src/main/java/com/openclassrooms/DataLayerSec/controller/UtilisateurController.java
+++ src/main/java/com/openclassrooms/DataLayerSec/controller/UtilisateurController.java
@@ -1,16 +1,26 @@
 package com.openclassrooms.DataLayerSec.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional; 
 
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
 import com.openclassrooms.DataLayerSec.service.UtilisateurService;
 import com.openclassrooms.DataLayerSec.service.UtilisateurService.EmailExistsException;
+import com.openclassrooms.DataLayerSec.service.UtilisateurService.InsufficientBalanceException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UtilisateurController {
@@ -67,5 +77,68 @@ public class UtilisateurController {
 		}
 
 		return "ajouter-ami";
+	}
+	
+
+	@GetMapping("/effectuerDepot")
+	public String showEffectuerDepotForm(@RequestParam Integer utilisateurId, HttpSession session, ModelMap model) {
+	    Optional<Utilisateur> optionalUtilisateur = utilisateurService.getUserById(utilisateurId);
+	    if (optionalUtilisateur.isPresent()) {
+	        Utilisateur utilisateur = optionalUtilisateur.get();
+	        session.setAttribute("utilisateur", utilisateur); // Stocker l'utilisateur dans l'attribut de session
+	        model.addAttribute("utilisateur", utilisateur);
+	        return "effectuerDepot";
+	    }
+	    return "erreur-utilisateur";
+	}
+
+	@PostMapping("/effectuerDepot")
+	public ModelAndView effectuerDepot(@RequestParam BigDecimal montant, HttpSession session) {
+	    Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur"); // Récupérer l'utilisateur depuis l'attribut de session
+	    
+	    if (utilisateur != null) {
+	        utilisateurService.effectuerDepot(utilisateur, montant);
+	        
+	        //ModelAndView modelAndView = new ModelAndView("redirect:/effectuerDepot?utilisateurId="+utilisateur.getUtilisateurId());
+	        ModelAndView modelAndView= new ModelAndView("operation-succes", "operation-succes", "Opération effectuée avec succès.");
+	        modelAndView.addObject("utilisateur", utilisateur);
+	        return modelAndView;
+	    } else {
+	    	
+	        return new ModelAndView("erreur-utilisateur", "erreur-utilisateur", "Utilisateur introuvable dans la session.");
+	     
+	}
+	}
+	
+	@PostMapping("/effectuerRetrait")
+	public ModelAndView effectuerRetrait(@RequestParam BigDecimal montant, HttpSession session) {
+	    Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+
+	    if (utilisateur != null) {
+	        try {
+	            utilisateurService.effectuerRetrait(utilisateur, montant);
+
+	           // ModelAndView modelAndView = new ModelAndView("redirect:/effectuerDepot?utilisateurId=" + utilisateur.getUtilisateurId());
+		        ModelAndView modelAndView= new ModelAndView("operation-succes", "operation-succes", "Opération effectuée avec succès.");
+	            modelAndView.addObject("utilisateur", utilisateur);
+	            return modelAndView;
+	        } catch (InsufficientBalanceException e) {
+	            return new ModelAndView("erreur-solde", "erreur-solde", "Solde insuffisant pour effectuer le retrait.");
+	        }
+	    } else {
+	        return new ModelAndView("erreur-utilisateur", "erreur-utilisateur", "Utilisateur introuvable dans la session.");
+	    }
+	}
+	
+	@GetMapping("/effectuerRetrait")
+	public String showEffectuerRetraitForm(@RequestParam Integer utilisateurId, HttpSession session, ModelMap model) {
+	    Optional<Utilisateur> optionalUtilisateur = utilisateurService.getUserById(utilisateurId);
+	    if (optionalUtilisateur.isPresent()) {
+	        Utilisateur utilisateur = optionalUtilisateur.get();
+	        session.setAttribute("utilisateur", utilisateur);
+	        model.addAttribute("utilisateur", utilisateur);
+	        return "effectuerRetrait";
+	    }
+	    return "erreur-utilisateur";
 	}
 }
