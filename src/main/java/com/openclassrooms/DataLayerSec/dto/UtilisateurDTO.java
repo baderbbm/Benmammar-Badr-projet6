bee3f51a462eed23f1778c098a537c9651426f52
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-31 00:28:40 +0200

    essai5

--- src/main/java/com/openclassrooms/DataLayerSec/dto/UtilisateurDTO.java
+++ src/main/java/com/openclassrooms/DataLayerSec/dto/UtilisateurDTO.java
@@ -1,1 +1,53 @@
 
+package com.openclassrooms.DataLayerSec.dto;
+
+import java.math.BigDecimal;
+
+public class UtilisateurDTO {
+
+    private String prenom;
+    private String nom;
+    private String motDePasse;
+    private String adresseEmail;
+    private BigDecimal soldeDuCompte;
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getAdresseEmail() {
+        return adresseEmail;
+    }
+
+    public void setAdresseEmail(String adresseEmail) {
+        this.adresseEmail = adresseEmail;
+    }
+
+    public BigDecimal getSoldeDuCompte() {
+        return soldeDuCompte;
+    }
+
+    public void setSoldeDuCompte(BigDecimal soldeDuCompte) {
+        this.soldeDuCompte = soldeDuCompte;
+    }
+}
