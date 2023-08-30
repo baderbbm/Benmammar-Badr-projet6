ae2aab1f1453d6fe960d3425a437900a5f6720b6
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-31 00:25:26 +0200

    Add files via upload

---
+++ src/main/java/com/openclassrooms/DataLayerSec/config/CustomUserDetails.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.DataLayerSec.config;
+
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import com.openclassrooms.DataLayerSec.model.Utilisateur;
+
+public class CustomUserDetails extends User {
+
+    private final Utilisateur utilisateur;
+
+    public CustomUserDetails(Utilisateur utilisateur) {
+        super(utilisateur.getAdresseEmail(), utilisateur.getMotDePasse(), new ArrayList<>()); 
+        this.utilisateur = utilisateur;
+    }
+}
