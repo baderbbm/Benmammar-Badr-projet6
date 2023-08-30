d7caaff673ead024acecd35e6fc8f55742b3af49
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-31 00:33:24 +0200

    essai5

--- src/main/java/com/openclassrooms/DataLayerSec/service/UtilisateurService.java
+++ src/main/java/com/openclassrooms/DataLayerSec/service/UtilisateurService.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.openclassrooms.DataLayerSec.dto.UtilisateurDTO;
 import com.openclassrooms.DataLayerSec.model.NatureOperation;
 import com.openclassrooms.DataLayerSec.model.Operation;
+import com.openclassrooms.DataLayerSec.model.Transfert;
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
 import com.openclassrooms.DataLayerSec.repository.OperationRepository;
+import com.openclassrooms.DataLayerSec.repository.TransfertRepository;
 import com.openclassrooms.DataLayerSec.repository.UtilisateurRepository;
  
 @Service
@@ -19,16 +23,22 @@ public class UtilisateurService {
 	@Autowired
 	private UtilisateurRepository utilisateurRepository;
 	
+	@Autowired
+	private TransfertRepository transfertRepository;
 	
 	@Autowired
 	private OperationRepository operationRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 	
 	public Iterable<Utilisateur> getUsers() {
 		return utilisateurRepository.findAll();
 	}
 	
-	public Optional<Utilisateur> getUserById(Integer id) {
+	public  Optional<Utilisateur>  getUserById(Integer id) {
 		return utilisateurRepository.findById(id); 
+		//return utilisateurRepository.findByUtilisateurId(id); 
 	}	
 	
 	
@@ -41,13 +51,15 @@ public class UtilisateurService {
 
 	
 	public Utilisateur addUtilisateur(Utilisateur utilisateur) {
-        try {
-            return utilisateurRepository.save(utilisateur);
-            // capturer et traiter spécifiquement l'exception liée à l'existence d'un emai
-        } catch (DataIntegrityViolationException ex) {
-            throw new EmailExistsException("L'email existe déjà.");
-        }
-    }
+	    try {
+	        String encryptedPassword = passwordEncoder.encode(utilisateur.getMotDePasse());
+	        utilisateur.setMotDePasse(encryptedPassword);
+	        return utilisateurRepository.save(utilisateur);
+	    } catch (DataIntegrityViolationException ex) {
+	        throw new EmailExistsException("L'email existe déjà.");
+	    }
+	}
+
 	
 	public void deleteUtilisateurById(Integer id) {
 		utilisateurRepository.deleteById(id);
@@ -74,12 +86,13 @@ public class UtilisateurService {
 	    }
 	    
 	    public void effectuerDepot(Utilisateur utilisateur, BigDecimal montant) {
-	        utilisateur.effectuerDepot(montant);
+	        BigDecimal montantAvecPrelevement = montant.multiply(BigDecimal.valueOf(0.995)); // 0.5% de prélèvement
+	        utilisateur.effectuerDepot(montantAvecPrelevement);
 	        utilisateurRepository.save(utilisateur);
 	        
 	        Operation operation = new Operation();
 	        operation.setUtilisateur(utilisateur);
-	        operation.setMontant(montant);
+	        operation.setMontant(montantAvecPrelevement);
 	        operation.setDateetheureoperation(LocalDateTime.now());
 	        operation.setNatureoperation(NatureOperation.depot);
 	        operationRepository.save(operation);
@@ -94,18 +107,71 @@ public class UtilisateurService {
 
 	    
 	    public void effectuerRetrait(Utilisateur utilisateur, BigDecimal montant) {
+	        BigDecimal montantAvecPrelevement = montant.multiply(BigDecimal.valueOf(0.995)); // 0.5% de prélèvement
 	        if (utilisateur.getSoldeDuCompte().compareTo(montant) >= 0) {
-	            utilisateur.effectuerRetrait(montant);
+	            utilisateur.effectuerRetrait(montantAvecPrelevement );
 	            utilisateurRepository.save(utilisateur);
 
 	            Operation operation = new Operation();
 	            operation.setUtilisateur(utilisateur);
-	            operation.setMontant(montant);
+	            operation.setMontant(montantAvecPrelevement);
 	            operation.setDateetheureoperation(LocalDateTime.now());
 	            operation.setNatureoperation(NatureOperation.retrait);
 	            operationRepository.save(operation);
 	        } else {
 	            throw new InsufficientBalanceException("Solde insuffisant pour effectuer le retrait.");
 	        }
-	    }	 
+	    }
+	    
+	    public Utilisateur convertToEntity(UtilisateurDTO utilisateurDTO) {
+	        Utilisateur utilisateur = new Utilisateur();
+	        utilisateur.setPrenom(utilisateurDTO.getPrenom());
+	        utilisateur.setNom(utilisateurDTO.getNom());
+	        utilisateur.setMotDePasse(utilisateurDTO.getMotDePasse());
+	        utilisateur.setAdresseEmail(utilisateurDTO.getAdresseEmail());
+	        utilisateur.setSoldeDuCompte(utilisateurDTO.getSoldeDuCompte());
+	        return utilisateur;
+	    }
+	    
+	    public UtilisateurDTO convertToDTO(Utilisateur utilisateur) {
+	        UtilisateurDTO utilisateurDTO = new UtilisateurDTO();
+	        utilisateurDTO.setPrenom(utilisateur.getPrenom());
+	        utilisateurDTO.setNom(utilisateur.getNom());
+	        utilisateurDTO.setMotDePasse(utilisateur.getMotDePasse());
+	        utilisateurDTO.setAdresseEmail(utilisateur.getAdresseEmail());
+	        utilisateurDTO.setSoldeDuCompte(utilisateur.getSoldeDuCompte());
+	        return utilisateurDTO;
+	    }
+	    
+	    public void effectuerVirement(String adresseEmailEmetteur, String adresseEmailBeneficiaire, BigDecimal montant) {
+	        BigDecimal montantAvecPrelevement = montant.multiply(BigDecimal.valueOf(0.995)); // 0.5% de prélèvement
+	        Utilisateur emetteur = findByAdresseEmail(adresseEmailEmetteur);
+	        Utilisateur beneficiaire = findByAdresseEmail(adresseEmailBeneficiaire);
+
+	        if (emetteur != null && beneficiaire != null) {
+	            BigDecimal soldeEmetteur = emetteur.getSoldeDuCompte();
+
+	            if (soldeEmetteur.compareTo(montant) >= 0) {
+	                emetteur.effectuerRetrait(montant);
+	                beneficiaire.effectuerDepot(montantAvecPrelevement);
+
+	                // Mise à jour des utilisateurs dans la base de données
+	                utilisateurRepository.save(emetteur);
+	                utilisateurRepository.save(beneficiaire);
+
+	                // Création et sauvegarde du transfert
+	                Transfert transfert = new Transfert();
+	                transfert.setUtilisateurEmetteur(emetteur);
+	                transfert.setUtilisateurBeneficiaire(beneficiaire);
+	                transfert.setMontant(montant);
+	                transfert.setDateHeureTransfert(LocalDateTime.now());
+
+	                transfertRepository.save(transfert);
+	            } else {
+	                throw new InsufficientBalanceException("Solde insuffisant pour effectuer le virement.");
+	            }
+	        } else {
+	            throw new IllegalArgumentException("Utilisateurs introuvables.");
+	        }
+	    }
 }
