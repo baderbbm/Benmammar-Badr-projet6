bb52935c9d2ffd8d96c87df726d6caa8eb4b4c8e
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-31 00:24:05 +0200

    essai5

--- src/main/java/com/openclassrooms/DataLayerSec/config/SecurityConfig.java
+++ src/main/java/com/openclassrooms/DataLayerSec/config/SecurityConfig.java
@@ -1,1 +1,55 @@
 
+package com.openclassrooms.DataLayerSec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
+    
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ 
+//définir l'enchaînement des filtres de sécurité qui sont appliqués aux demandes entrantes dans l'application. 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/login", "/enregistrerUtilisateur").permitAll() 
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/process-login")
+                .defaultSuccessUrl("/home", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                  .logout()
+                  .logoutSuccessUrl("/login?logout=true")
+                  .invalidateHttpSession(true)
+                  .deleteCookies("JSESSIONID")
+                  .permitAll()
+                  .and()
+                  .csrf()
+                  .disable();
+
+        return http.build();
+    }  
+}
