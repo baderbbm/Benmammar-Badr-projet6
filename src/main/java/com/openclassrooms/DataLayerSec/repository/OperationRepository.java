abaff15d9ae69e5594f1418ff68745db9bb199cd
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-31 00:31:27 +0200

    essai5

--- src/main/java/com/openclassrooms/DataLayerSec/repository/OperationRepository.java
+++ src/main/java/com/openclassrooms/DataLayerSec/repository/OperationRepository.java
@@ -1,13 +1,13 @@
 package com.openclassrooms.DataLayerSec.repository;
 
-
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.DataLayerSec.model.Operation;
+import com.openclassrooms.DataLayerSec.model.Utilisateur;
 
 @Repository
-public interface OperationRepository extends CrudRepository<Operation, Integer> {
-	
-	
+public interface OperationRepository extends JpaRepository<Operation, Integer> {
+    List<Operation> findByUtilisateur(Utilisateur utilisateur);
 }
