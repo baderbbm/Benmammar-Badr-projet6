13582c18c7fc03210030a7508f8eceb146543a0b
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-31 00:24:38 +0200

    Add files via upload

---
+++ src/main/java/com/openclassrooms/DataLayerSec/config/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.openclassrooms.DataLayerSec.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.DataLayerSec.model.Utilisateur;
+import com.openclassrooms.DataLayerSec.repository.UtilisateurRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UtilisateurRepository utilisateurRepository) {
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Utilisateur utilisateur = utilisateurRepository.findByAdresseEmail(email);
+        if (utilisateur == null) {
+            throw new UsernameNotFoundException("Utilisateur introuvable avec cet e-mail : " + email);
+        }        
+        return new CustomUserDetails(utilisateur);
+    }
+}
