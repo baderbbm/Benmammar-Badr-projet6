e172f5e79931af12040c4fe2bfb2ec66015bf363
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-09-05 00:20:17 +0200

    essai8

--- src/main/java/com/openclassrooms/DataLayerSec/service/OperationService.java
+++ src/main/java/com/openclassrooms/DataLayerSec/service/OperationService.java
@@ -3,6 +3,8 @@ package com.openclassrooms.DataLayerSec.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.openclassrooms.DataLayerSec.dto.UtilisateurDTO;
 import com.openclassrooms.DataLayerSec.model.Operation;
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
 import com.openclassrooms.DataLayerSec.repository.OperationRepository;
@@ -13,11 +15,15 @@ public class OperationService {
     private final OperationRepository operationRepository;
 
     @Autowired
+	private UtilisateurService utilisateurService;
+    
+    @Autowired
     public OperationService(OperationRepository operationRepository) {
         this.operationRepository = operationRepository;
     }
 
-    public List<Operation> findByUtilisateur(Utilisateur utilisateur) {
+    public List<Operation> findByUtilisateur(UtilisateurDTO utilisateurDTO) {
+		Utilisateur utilisateur=utilisateurService.findByAdresseEmail(utilisateurDTO.getAdresseEmail());		
         return operationRepository.findByUtilisateur(utilisateur);
     }
 }
