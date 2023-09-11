f0370999d6574c4ff84cb9a9eb12ba76ab0ee929
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-09-11 15:47:55 +0200

    essai9

--- src/main/java/com/openclassrooms/DataLayerSec/config/CustomUserDetails.java
+++ src/main/java/com/openclassrooms/DataLayerSec/config/CustomUserDetails.java
@@ -5,11 +5,9 @@ import org.springframework.security.core.userdetails.User;
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
 
 public class CustomUserDetails extends User {
-
     private final Utilisateur utilisateur;
-
     public CustomUserDetails(Utilisateur utilisateur) {
-        super(utilisateur.getAdresseEmail(), utilisateur.getMotDePasse(), new ArrayList<>()); 
+    	super(utilisateur.getAdresseEmail(), utilisateur.getMotDePasse(), new ArrayList<>()); 
         this.utilisateur = utilisateur;
     }
 }
