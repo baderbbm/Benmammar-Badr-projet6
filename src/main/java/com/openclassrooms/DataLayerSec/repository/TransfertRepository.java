4b5f1b54d0afda8b32a2b8ab9e2abe6f7f430f4b
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-09-14 14:14:16 +0200

    essai9

--- src/main/java/com/openclassrooms/DataLayerSec/repository/TransfertRepository.java
+++ src/main/java/com/openclassrooms/DataLayerSec/repository/TransfertRepository.java
@@ -4,10 +4,7 @@ package com.openclassrooms.DataLayerSec.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import com.openclassrooms.DataLayerSec.model.Operation;
 import com.openclassrooms.DataLayerSec.model.Transfert;
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
 
