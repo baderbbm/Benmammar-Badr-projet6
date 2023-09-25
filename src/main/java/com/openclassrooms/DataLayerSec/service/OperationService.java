f5404e1d5aaabdef71516e0db381409f8b5206af
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-09-25 21:20:06 +0200

    essai11

--- src/main/java/com/openclassrooms/DataLayerSec/service/OperationService.java
+++ src/main/java/com/openclassrooms/DataLayerSec/service/OperationService.java
@@ -14,39 +14,42 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class OperationService {
-    
-    private final OperationRepository operationRepository;
 
-    @Autowired
+	private final OperationRepository operationRepository;
+
+	@Autowired
 	private UtilisateurService utilisateurService;
-    
-    @Autowired
-    public OperationService(OperationRepository operationRepository) {
-        this.operationRepository = operationRepository;
-    }
 
-    public List<Operation> findByUtilisateur(UtilisateurDTO utilisateurDTO) {
-		Utilisateur utilisateur=utilisateurService.findByAdresseEmail(utilisateurDTO.getAdresseEmail());		
-        return operationRepository.findByUtilisateur(utilisateur);
-    }
+	@Autowired
+	public OperationService(OperationRepository operationRepository) {
+		this.operationRepository = operationRepository;
+	}
 
-    public List<OperationDTO> findByUtilisateurDTO(UtilisateurDTO utilisateurDTO) {
-          return convertToOperationDTOList(findByUtilisateur(utilisateurDTO));
-      }
-   
-public static List<OperationDTO> convertToOperationDTOList(List<Operation> operations) {
-    List<OperationDTO> operationDTOList = new ArrayList<>();
+	// Récupère la liste des opérations effectuées par un utilisateur donné
+	public List<Operation> findByUtilisateur(UtilisateurDTO utilisateurDTO) {
+		Utilisateur utilisateur = utilisateurService.findByAdresseEmail(utilisateurDTO.getAdresseEmail());
+		return operationRepository.findByUtilisateur(utilisateur);
+	}
 
-    for (Operation operation : operations) {
-        OperationDTO operationDTO = new OperationDTO();
-        operationDTO.setOperationId(operation.getOperationId());
-         UtilisateurDTO utilisateurDTO = new UtilisateurDTO();
-        operationDTO.setUtilisateur(utilisateurDTO); 
-        operationDTO.setMontant(operation.getMontant());
-        operationDTO.setDateetheureoperation(operation.getDateetheureoperation());
-        operationDTO.setNatureoperation(operation.getNatureoperation());
-        operationDTOList.add(operationDTO);
-    }
-    return operationDTOList;
-}
+	// Récupère la liste des opérations effectuées par un utilisateur donné sous
+	// forme de DTO
+	public List<OperationDTO> findByUtilisateurDTO(UtilisateurDTO utilisateurDTO) {
+		return convertToOperationDTOList(findByUtilisateur(utilisateurDTO));
+	}
+
+	// Convertit une liste d'entités Operation en une liste de DTO OperationDTO
+	public static List<OperationDTO> convertToOperationDTOList(List<Operation> operations) {
+		List<OperationDTO> operationDTOList = new ArrayList<>();
+		for (Operation operation : operations) {
+			OperationDTO operationDTO = new OperationDTO();
+			operationDTO.setOperationId(operation.getOperationId());
+			UtilisateurDTO utilisateurDTO = new UtilisateurDTO();
+			operationDTO.setUtilisateur(utilisateurDTO);
+			operationDTO.setMontant(operation.getMontant());
+			operationDTO.setDateetheureoperation(operation.getDateetheureoperation());
+			operationDTO.setNatureoperation(operation.getNatureoperation());
+			operationDTOList.add(operationDTO);
+		}
+		return operationDTOList;
+	}
 }
