3104d64e62da42c090636600574d386f34f141d1
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-09-25 21:18:25 +0200

    essai11

--- src/main/java/com/openclassrooms/DataLayerSec/service/TransfertService.java
+++ src/main/java/com/openclassrooms/DataLayerSec/service/TransfertService.java
@@ -1,5 +1,5 @@
 package com.openclassrooms.DataLayerSec.service;
- 
+
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,43 +10,48 @@ import com.openclassrooms.DataLayerSec.model.Transfert;
 import com.openclassrooms.DataLayerSec.model.Utilisateur;
 import com.openclassrooms.DataLayerSec.repository.TransfertRepository;
 import jakarta.transaction.Transactional;
- 
+
 @Service
 @Transactional
 public class TransfertService {
- 
+
 	@Autowired
 	private TransfertRepository transfertRepository;
-	 @Autowired
-		private UtilisateurService utilisateurService;
-	
+	@Autowired
+	private UtilisateurService utilisateurService;
+
+	// Récupère la liste des transferts effectués par un utilisateur émetteur donné
 	public List<Transfert> getVirementsByUtilisateurEmetteur(UtilisateurDTO utilisateurEmetteurDTO) {
-		Utilisateur utilisateurEmetteur=utilisateurService.findByAdresseEmail(utilisateurEmetteurDTO.getAdresseEmail());		
-        return transfertRepository.findByUtilisateurEmetteur(utilisateurEmetteur);
-    }
-	
-	public List<TransfertDTO> getVirementsByUtilisateurEmetteurDTO(UtilisateurDTO utilisateurEmetteurDTO) {
-			
-        return convertToTransfertDTOList(getVirementsByUtilisateurEmetteur(utilisateurEmetteurDTO));
-    }
-	
-	public static List<TransfertDTO> convertToTransfertDTOList(List<Transfert> transferts) {
-        List<TransfertDTO> transfertDTOList = new ArrayList<>();
-        for (Transfert transfert : transferts) {
-            TransfertDTO transfertDTO = new TransfertDTO();
-            transfertDTO.setTransfertId(transfert.getTransfertId());
-            UtilisateurDTO emetteurDTO = new UtilisateurDTO();
-            transfertDTO.setUtilisateurEmetteurDTO(emetteurDTO); 
-            UtilisateurDTO beneficiaireDTO = new UtilisateurDTO();
-            Utilisateur utilisateurBeneficiaire = transfert.getUtilisateurBeneficiaire();
-            if (utilisateurBeneficiaire != null)
-            beneficiaireDTO.setNom(utilisateurBeneficiaire.getNom());
-            transfertDTO.setUtilisateurBeneficiaireDTO(beneficiaireDTO); 
-            transfertDTO.setMontant(transfert.getMontant());
-            transfertDTO.setDateHeureTransfert(transfert.getDateHeureTransfert());
-            transfertDTO.setDescription(transfert.getDescription());
-            transfertDTOList.add(transfertDTO);
-        }
-        return transfertDTOList;
-    }
+		Utilisateur utilisateurEmetteur = utilisateurService
+				.findByAdresseEmail(utilisateurEmetteurDTO.getAdresseEmail());
+		return transfertRepository.findByUtilisateurEmetteur(utilisateurEmetteur);
 	}
+
+	// Récupère la liste des transferts effectués par un utilisateur émetteur donné
+	// sous forme de DTO
+	public List<TransfertDTO> getVirementsByUtilisateurEmetteurDTO(UtilisateurDTO utilisateurEmetteurDTO) {
+
+		return convertToTransfertDTOList(getVirementsByUtilisateurEmetteur(utilisateurEmetteurDTO));
+	}
+
+	// Convertit une liste d'entités Transfert en une liste de DTO TransfertDTO
+	public static List<TransfertDTO> convertToTransfertDTOList(List<Transfert> transferts) {
+		List<TransfertDTO> transfertDTOList = new ArrayList<>();
+		for (Transfert transfert : transferts) {
+			TransfertDTO transfertDTO = new TransfertDTO();
+			transfertDTO.setTransfertId(transfert.getTransfertId());
+			UtilisateurDTO emetteurDTO = new UtilisateurDTO();
+			transfertDTO.setUtilisateurEmetteurDTO(emetteurDTO);
+			UtilisateurDTO beneficiaireDTO = new UtilisateurDTO();
+			Utilisateur utilisateurBeneficiaire = transfert.getUtilisateurBeneficiaire();
+			if (utilisateurBeneficiaire != null)
+				beneficiaireDTO.setNom(utilisateurBeneficiaire.getNom());
+			transfertDTO.setUtilisateurBeneficiaireDTO(beneficiaireDTO);
+			transfertDTO.setMontant(transfert.getMontant());
+			transfertDTO.setDateHeureTransfert(transfert.getDateHeureTransfert());
+			transfertDTO.setDescription(transfert.getDescription());
+			transfertDTOList.add(transfertDTO);
+		}
+		return transfertDTOList;
+	}
+}
